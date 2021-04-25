47ecb5cd0006529a17aadfc25335936ba135d80e
AnastasisGi <anastasiosgks@gmail.com>
2021-04-25 12:35:13 +0100

    Method for withdraw money is not passing

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -10,4 +10,12 @@ RSpec.describe Bank do
       expect(bank.deposit(1000)).to eq(1000)
     end
   end
+
+  context('When withdraing money ') do 
+    it('Lowers the amount ') do 
+        bank.deposit(1000)
+        bank.withdraw(1)
+        expect(bank.balance).to eq(999)
+    end
+  end
 end
